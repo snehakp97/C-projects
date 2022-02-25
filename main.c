472647d218741b07488d36c9605bfa4e0685fa1e
Cloud User <ec2-user@ip-172-31-95-103.ec2.internal>
2022-02-25 10:55:28 +0000

    added ciprojects

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void  biggest(void);
+void         factorial(void);
+void        reverse(void);
+int main() {
+
+         biggest();
+         factorial();
+        reverse();
+	return 0;
+}
